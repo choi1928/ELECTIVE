608c569d9558d6ad04ac47b4de1175876801258b
choi1928 <arieltungol20@gmail.com>
2025-09-02 01:23:22 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace IT_ELECTIVES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtemp = new System.Windows.Forms.TextBox();
+            this.txtfir = new System.Windows.Forms.TextBox();
+            this.txtlas = new System.Windows.Forms.TextBox();
+            this.txtmid = new System.Windows.Forms.TextBox();
+            this.txtgen = new System.Windows.Forms.TextBox();
+            this.txtpos = new System.Windows.Forms.TextBox();
+            this.txtdep = new System.Windows.Forms.TextBox();
+            this.txtcon = new System.Windows.Forms.TextBox();
+            this.txtadd = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Edit = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtedit = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtemp
+            // 
+            this.txtemp.Location = new System.Drawing.Point(440, 158);
+            this.txtemp.Name = "txtemp";
+            this.txtemp.Size = new System.Drawing.Size(100, 20);
+            this.txtemp.TabIndex = 0;
+            this.txtemp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtemp_KeyDown);
+            // 
+            // txtfir
+            // 
+            this.txtfir.Location = new System.Drawing.Point(440, 207);
+            this.txtfir.Name = "txtfir";
+            this.txtfir.Size = new System.Drawing.Size(100, 20);
+            this.txtfir.TabIndex = 1;
+            this.txtfir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtfir_KeyDown);
+            // 
+            // txtlas
+            // 
+            this.txtlas.Location = new System.Drawing.Point(440, 253);
+            this.txtlas.Name = "txtlas";
+            this.txtlas.Size = new System.Drawing.Size(100, 20);
+            this.txtlas.TabIndex = 2;
+            this.txtlas.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtlast_KeyDown);
+            // 
+            // txtmid
+            // 
+            this.txtmid.Location = new System.Drawing.Point(440, 302);
+            this.txtmid.Name = "txtmid";
+            this.txtmid.Size = new System.Drawing.Size(100, 20);
+            this.txtmid.TabIndex = 3;
+            this.txtmid.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtmid_KeyDown);
+            // 
+            // txtgen
+            // 
+            this.txtgen.Location = new System.Drawing.Point(440, 352);
+            this.txtgen.Name = "txtgen";
+            this.txtgen.Size = new System.Drawing.Size(100, 20);
+            this.txtgen.TabIndex = 4;
+            this.txtgen.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtgen_KeyDown);
+            // 
+            // txtpos
+            // 
+            this.txtpos.Location = new System.Drawing.Point(440, 402);
+            this.txtpos.Name = "txtpos";
+            this.txtpos.Size = new System.Drawing.Size(100, 20);
+            this.txtpos.TabIndex = 5;
+            this.txtpos.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtpos_KeyDown);
+            // 
+            // txtdep
+            // 
+            this.txtdep.Location = new System.Drawing.Point(440, 448);
+            this.txtdep.Name = "txtdep";
+            this.txtdep.Size = new System.Drawing.Size(100, 20);
+            this.txtdep.TabIndex = 6;
+            this.txtdep.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtdep_KeyDown);
+            // 
+            // txtcon
+            // 
+            this.txtcon.Location = new System.Drawing.Point(440, 499);
+            this.txtcon.Name = "txtcon";
+            this.txtcon.Size = new System.Drawing.Size(100, 20);
+            this.txtcon.TabIndex = 7;
+            this.txtcon.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtcon_KeyDown);
+            // 
+            // txtadd
+            // 
+            this.txtadd.Location = new System.Drawing.Point(440, 550);
+            this.txtadd.Name = "txtadd";
+            this.txtadd.Size = new System.Drawing.Size(100, 20);
+            this.txtadd.TabIndex = 8;
+            this.txtadd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtadd_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 590);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(931, 150);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(118, 762);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(772, 759);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Employee No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(332, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "FirstName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(332, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "LastName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(332, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "MiddleName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(332, 355);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(332, 409);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Position";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(332, 451);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Department";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(332, 502);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Contact";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(332, 557);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 15);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Address";
+            // 
+            // Edit
+            // 
+            this.Edit.AutoSize = true;
+            this.Edit.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit.Location = new System.Drawing.Point(50, 767);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(52, 15);
+            this.Edit.TabIndex = 21;
+            this.Edit.Text = "Search";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(246, 762);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 23;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtedit
+            // 
+            this.txtedit.Location = new System.Drawing.Point(118, 798);
+            this.txtedit.Name = "txtedit";
+            this.txtedit.Size = new System.Drawing.Size(100, 20);
+            this.txtedit.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 830);
+            this.Controls.Add(this.txtedit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtadd);
+            this.Controls.Add(this.txtcon);
+            this.Controls.Add(this.txtdep);
+            this.Controls.Add(this.txtpos);
+            this.Controls.Add(this.txtgen);
+            this.Controls.Add(this.txtmid);
+            this.Controls.Add(this.txtlas);
+            this.Controls.Add(this.txtfir);
+            this.Controls.Add(this.txtemp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtemp;
+        private System.Windows.Forms.TextBox txtfir;
+        private System.Windows.Forms.TextBox txtlas;
+        private System.Windows.Forms.TextBox txtmid;
+        private System.Windows.Forms.TextBox txtgen;
+        private System.Windows.Forms.TextBox txtpos;
+        private System.Windows.Forms.TextBox txtdep;
+        private System.Windows.Forms.TextBox txtcon;
+        private System.Windows.Forms.TextBox txtadd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Edit;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtedit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace IT_ELECTIVES
+{
+    public partial class Form1 : Form
+    {
+        private string connStr;
+        MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connStr);
+            LoadEmployees();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadEmployees();
+        }
+
+        private void txtemp_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtfir.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtfir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtlas.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtlast_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtmid.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtmid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtgen.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtgen_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtpos.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtpos_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtdep.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtdep_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtcon.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtcon_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtadd.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtadd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SaveEmployee(); // ✅ call save function
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=localhost;user=root;database=employee_db;password=;";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = @"UPDATE employees 
+               SET first_name=@fir, last_name=@las, middle_name=@mid, 
+                   gender=@gen, position=@pos, department=@dep, 
+                   contact=@con, address=@add 
+               WHERE emp_no=@emp";
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    // ✅ Make sure emp_no is INT (if table is INT)
+                    cmd.Parameters.AddWithValue("@emp", Convert.ToInt32(txtedit.Text));
+
+                    cmd.Parameters.AddWithValue("@fir", txtfir.Text);
+                    cmd.Parameters.AddWithValue("@las", txtlas.Text);
+                    cmd.Parameters.AddWithValue("@mid", txtmid.Text);
+                    cmd.Parameters.AddWithValue("@gen", txtgen.Text);
+                    cmd.Parameters.AddWithValue("@pos", txtpos.Text);
+                    cmd.Parameters.AddWithValue("@dep", txtdep.Text);
+                    cmd.Parameters.AddWithValue("@con", txtcon.Text);
+                    cmd.Parameters.AddWithValue("@add", txtadd.Text);
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Employee updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadEmployees(); // refresh DataGridView
+                    }
+                    else
+                    {
+                        MessageBox.Show("No employee updated. Check the emp_no in Edit textbox.",
+                                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error updating employee: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                    txtemp.Text = row.Cells["emp_no"].Value.ToString();
+                    txtfir.Text = row.Cells["first_name"].Value.ToString();
+                    txtlas.Text = row.Cells["last_name"].Value.ToString();
+                    txtmid.Text = row.Cells["middle_name"].Value.ToString();
+                    txtgen.Text = row.Cells["gender"].Value.ToString();
+                    txtpos.Text = row.Cells["position"].Value.ToString();
+                    txtdep.Text = row.Cells["department"].Value.ToString();
+                    txtcon.Text = row.Cells["contact"].Value.ToString();
+                    txtadd.Text = row.Cells["address"].Value.ToString();
+
+                  
+                }
+            }
+
+
+        }
+        // ✅ Load Employees into DataGridView
+        private void LoadEmployees(string search = "")
+        {
+            string connStr = "server=localhost;user=root;database=employee_db;password=;";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM employees";
+
+                    if (!string.IsNullOrEmpty(search))
+                    {
+                        query += " WHERE emp_no LIKE @search OR first_name LIKE @search OR last_name LIKE @search";
+                    }
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        if (!string.IsNullOrEmpty(search))
+                        {
+                            cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+                        }
+
+                        MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading employees: " + ex.Message);
+                }
+            }
+        }
+
+        // ✅ Clear all textboxes
+        private void ClearBoxes()
+        {
+           
+        }
+        private void SaveEmployee()
+        {
+            {
+                string connStr = "server=localhost;user=root;database=employee_db;password=;";
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string sql = "INSERT INTO employees (emp_no, first_name, last_name, middle_name, gender, position, department, contact, address) " +
+                                     "VALUES (@emp, @fir, @las, @mid, @gen, @pos, @dep, @con, @add)";
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                        cmd.Parameters.AddWithValue("@emp", txtemp.Text);
+                        cmd.Parameters.AddWithValue("@fir", txtfir.Text);
+                        cmd.Parameters.AddWithValue("@las", txtlas.Text);
+                        cmd.Parameters.AddWithValue("@mid", txtmid.Text);
+                        cmd.Parameters.AddWithValue("@gen", txtgen.Text);
+                        cmd.Parameters.AddWithValue("@pos", txtpos.Text);
+                        cmd.Parameters.AddWithValue("@dep", txtdep.Text);
+                        cmd.Parameters.AddWithValue("@con", txtcon.Text);
+                        cmd.Parameters.AddWithValue("@add", txtadd.Text);
+
+                        int rows = cmd.ExecuteNonQuery();
+
+                        if (rows > 0)
+                        {
+                            MessageBox.Show("Employee saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LoadEmployees(); // refresh DataGridView
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to save employee.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error saving employee: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=localhost;user=root;database=employee_db;password=;";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM employees WHERE emp_no LIKE @search OR first_name LIKE @search OR last_name LIKE @search";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@search", "%" + txtSearch.Text + "%");
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error searching: " + ex.Message);
+                }
+            }
+        }
+
+        private void txtedit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
